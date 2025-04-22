4ed654f17e98b7d959eedcbb24162e8a0eba7849
Adnan Qureshi <72442243+Sparrow-13@users.noreply.github.com>
2025-04-23 02:43:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (9.35%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,16 +1,6 @@
 class Solution {
     public String toLowerCase(String s) {
-        var arr = s.toCharArray();
-        var str = new StringBuilder();
-        for(var c : arr){
-            if(c>=65 && c <=90){
-                c += 32;
-                str.append(c);
-                continue;
-            }
-            str.append(c);
-
-        }
-        return str.toString();
+    
+        return s.toLowerCase();
     }
 }
