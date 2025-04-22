17d636c05eedb4ba3e12df447fbddf2d12389cc3
Adnan Qureshi <72442243+Sparrow-13@users.noreply.github.com>
2025-04-23 03:03:29 +0530

    Time: 4 ms (96.51%) | Memory: 44.8 MB (21.27%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        var x = 0;
+        var y = 0;
+
+        for(var c : moves.toCharArray()){
+            if(c == 'U'){
+                y+=1;
+            }
+            else if(c == 'D'){
+                y -= 1;
+            }
+            else if(c == 'L'){
+                x-=1;
+            }
+            else{
+                x+= 1;
+            }
+        }
+        return x == 0 && y==0;
+    }
+}
