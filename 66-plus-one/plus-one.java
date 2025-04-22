39bded1a5c5cb5b7d9ca5d118b4656bafc6c2b42
Adnan Qureshi <72442243+Sparrow-13@users.noreply.github.com>
2025-04-22 23:25:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (66.75%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static int[] plusOne(int[] digits) {
+		var carry = 1;
+		var n = digits.length - 1;
+		while (n >= 0) {
+			if ((digits[n] + carry) == 10) {
+				carry = 1;
+				digits[n] = 0;
+				n--;
+				continue;
+			}
+			carry = 0;
+			digits[n]++;
+			break;
+		}
+		if (carry == 1) {
+			var arr = new int[digits.length + 1];
+			arr[0] = 1;
+			for (int i = 1; i <= digits.length; i++) {
+				arr[i] = digits[i - 1];
+			}
+			return arr;
+		}
+		return digits;
+	}
+}
